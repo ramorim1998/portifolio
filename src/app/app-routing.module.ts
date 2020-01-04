7e93ef57fabcfa670827b038c3c8daa0225bbb4b
Rafael <araujor920@gmail.com>
2020-01-04 10:52:14 -0300

    criação parcial da tela home e da barra de navegação

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { CurriculumComponent } from './curriculum/curriculum.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {path:'', redirectTo:'home',pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'curriculum-vitae',component:CurriculumComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { CurriculumComponent } from './curriculum/curriculum.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CurriculumComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
